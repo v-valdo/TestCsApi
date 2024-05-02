08c1fa903ca5336e98103dad6794768e44c81fd0
v-valdo <v.sersam@gmail.com>
2024-05-02 09:48:49 +0200

    update dyndata and empty utils class

---
+++ Backend/src/Utils.cs
@@ -0,0 +1,11 @@
+public static class Utils
+{
+    public static bool IsPasswordStrongEnough(string password)
+    {
+        return false;
+    }
+    public static string RemoveBadWords(string text)
+    {
+        return "VeryBadWord";
+    }
+}
