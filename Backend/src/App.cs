9757f5143420bff67de922bfb178c770f02c9805
VValdo <v.sersam@gmail.com>
2024-05-07 11:55:54 +0200

    removed comments

--- Backend/src/App.cs
+++ Backend/src/App.cs
@@ -11,13 +11,6 @@ Globals = Obj(new
     sessionLifeTimeHours = 2
 });
 
-// Example of how you can run tests as if they were part of the main program
-// This solves the problem with console output not working when running tests
-// using "dotnet test". But in the end we want to run the tests by calling
-// "dotnet test" - so it's only useful during the development of the tests!
-// Remove the comments for this line and comment out "Server.Start()":
-// new UtilsTest().TestCreateMockUsers();
-
 // Server.Start();
 
 WebApp.Utils.CreateMockUsers();
